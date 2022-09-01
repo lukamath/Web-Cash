e5529aa314108f7f8a31077a9bb1455ce8772d99
lukamath <lukamath@cella1.com>
2022-09-01 19:10:14 +0200

    some classes

--- application.py
+++ application.py
@@ -9,5 +9,32 @@ app.config['SECRET_KEY']='uOzPG137aJNoq2bBJ4b9P81DY5vCiRXj'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///db_billing.db'
 db=SQLAlchemy(app)	
 
+class User(db.Model):
+	id=db.Column(db.Integer, primary_key=True)
+	name=db.Column(db.String(25), nullable=False)
+	surname=db.Column(db.String(25), nullable=False)
+	username=db.Column(db.String(13),nullable=False)
+	password=db.Column(db.String(13),nullable=False)
 
+class Receipt(db.Model):
+	id=db.Column(db.Integer, primary_key=True)
+	customer_id=db.Column(db.Integer, db.ForeignKey('customer.id'))
+	payment_id=db.Column(db.Integer, db.ForeignKey('payment.id'))
+	date_issue=db.Column(db.DateTime, nullable=False)
+	total=db.Column(db.Numeric, nullable=False)
 
+class Payment(db.Model):
+	id=db.Column(db.Integer, primary_key=True)
+	receipt_id=db.Column(db.Integer, db.ForeignKey('receipt.id'))
+	receipt=db.relationship('Receipt',backref='payment')
+
+class Customer(db.Model):
+	id=db.Column(db.Integer, primary_key=True)
+	name=db.Column(db.String(25), nullable=False)
+	surname=db.Column(db.String(25), nullable=False)
+	tax_code=db.Column(db.String(16), nullable=False)
+	receipts=db.relationship('Receipt',backref='customer', lazy=True)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
